08b145be1f0afcfc4c556443ef44e2c33f66c81e
Andrés Murray Roppel <andymurray99@hotmail.com>
2026-02-14 13:19:37 -0300

    fix en settings para el manejo de https de railway

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -176,6 +176,7 @@ CORS_ALLOW_HEADERS = [
 
 # Security settings for production
 if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
